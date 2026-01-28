65f8be6c07259f9f25f583842992987fdaf35c3a
ptvidal <Patricio.Vidal@siigroup.cl>
2026-01-28 20:38:51 -0300

    Fix Lambda handler to match lambda_function.py

--- lambda.tf
+++ lambda.tf
@@ -67,7 +67,7 @@ resource "aws_lambda_function" "main" {
   filename         = data.archive_file.lambda_placeholder.output_path
   function_name    = "${var.project_name}-function"
   role            = aws_iam_role.lambda_role.arn
-  handler         = "index.handler"
+  handler         = "lambda_function.lambda_handler"
   source_code_hash = data.archive_file.lambda_placeholder.output_base64sha256
   runtime         = var.lambda_runtime
   timeout         = var.lambda_timeout
